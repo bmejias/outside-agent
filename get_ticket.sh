40d7e4ab25e0604779ad024ff1a57a547f5ee2d5
Boriss Mejias <bmejias@dcc.uchile.cl>
2015-05-05 18:04:00 +0200

    removing unneeded variables

--- get_ticket.sh
+++ get_ticket.sh
@@ -3,8 +3,7 @@
 username=`./get_csv_item.sh $1 username`
 password=`./get_csv_item.sh $1 password`
 alf_url=`./get_csv_item.sh $1 url`
-api_url="${alf_url}/alfresco/service/api"
-login_url="${api_url}/login"
+login_url="${alf_url}/alfresco/service/api/login"
 
 ticket=`curl -G "${login_url}" --data-urlencode "u=${username}" --data-urlencode "pw=${password}" 2> /dev/null | xmllint --xpath '/ticket/text()' -`
 echo $ticket
--- get_version.sh
+++ get_version.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 alf_url=`./get_csv_item.sh $1 url`
-api_url="${alf_url}/alfresco/service/api"
-server_data_url="${api_url}/server"
+server_data_url="${alr_url}/alfresco/service/api/server"
 
 ticket=`./get_ticket.sh $1`
 result=`curl -G "${server_data_url}?alf_ticket=${ticket}" 2> /dev/null | jshon -e data -e version -`
