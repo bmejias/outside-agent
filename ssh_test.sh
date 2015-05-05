d207e4e3bc3434fb488495ae42361d139ea733dc
Boriss Mejias <bmejias@dcc.uchile.cl>
2015-05-05 17:03:40 +0200

    make ssh working with more general config

--- ssh_test.sh
+++ ssh_test.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-(ssh -l $OAUSER -i $OAKEY $1 /bin/true 2> /dev/null && echo "$1 success") || echo "$1 failed"
+source config_ssh
+
+ip=`./get_csv_item.sh $1 ip`
+(ssh -l $OAUSER -i $OAKEY $ip /bin/true 2> /dev/null && echo "$1 success") || echo "$1 failed"
